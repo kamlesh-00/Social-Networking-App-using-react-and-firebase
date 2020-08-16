455d37aad7640cf3f7e5d94723c421cc139a81cb
Kamlesh Mistry <mistrykamlesh312@gmail.com>
2020-08-16 12:44:07 +0530

    Firebase authentication added

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -1,13 +1,28 @@
 const functions = require('firebase-functions');
-
 const admin = require('firebase-admin');
+const app = require('express')();
+
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const firebaseConfig = {
+    apiKey: "AIzaSyAXEhxTCIsRw0E8BhGWsFoqeqoLGQaOT58",
+    authDomain: "socialape-7dafe.firebaseapp.com",
+    databaseURL: "https://socialape-7dafe.firebaseio.com",
+    projectId: "socialape-7dafe",
+    storageBucket: "socialape-7dafe.appspot.com",
+    messagingSenderId: "36129473466",
+    appId: "1:36129473466:web:2b2532b74bff0198fe1383",
+    measurementId: "G-VRJXSDBL0L"
+};
+
+const firebase = require('firebase');
+const { user } = require('firebase-functions/lib/providers/auth');
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
 
 app.get('/screams',(req,res)=>{
-    admin.firestore().collection('screams')
+    db.collection('screams')
         .orderBy('createdAt','desc')
         .get()
         .then(data=>{
@@ -17,9 +32,9 @@ app.get('/screams',(req,res)=>{
                     screamId: doc.id,
                     body: doc.data().body,
                     userHandle: doc.data().userHandle,
-                    createdAt: doc.data().createdAt,
-                    commentCount: doc.data().commentCount,
-                    likeCount: doc.data().likeCount()
+                    // createdAt: doc.data().createdAt,
+                    // commentCount: doc.data().commentCount,
+                    // likeCount: doc.data().likeCount()
                 })
             });
             return res.json(screams)
@@ -36,14 +51,43 @@ app.post('/addScream',(req,res)=>{
         userHandle: req.body.userHandle,
         createdAt: new Date().toISOString()
     };
-    admin.firestore().collection('screams').add(newScream)
+    db.collection('screams').add(newScream)
     .then(doc=>{
         return res.json({message:`document ${doc.id} created successfully.`})
     })
     .catch(err=>{
         res.status(500).json({error:`Something went wrong.`});
-        console.log("Error: "+err);
+        console.log(err);
     });
 });
 
-exports.api = functions.https.onRequest(app);
+//Signup route
+app.post('/signup',(req,res)=>{
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle
+    };
+
+    db.doc(`/users/${newUser.handle}`).get()
+    .then(doc=>{
+        if(doc.exists){
+            return res.status(400).json({handle: "This handle is already taken"})
+        }else{
+            firebase.auth().createUserWithEmailAndPassword(newUser.email,newUser.password);
+        }
+    })
+    .then(data=>{
+        return data.user.getIdToken();
+    })
+    .then(token=>{
+        return res.status(201).json({token});
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({error:err.code});
+    });
+});
+
+exports.api = functions.regios('europe-west1').https.onRequest(app);
